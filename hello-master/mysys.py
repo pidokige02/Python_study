d1157f1b01822a09b5c2bc763190f5a3b3e01c88
pidokige02 <43601998+pidokige02@users.noreply.github.com>
2021-04-25 16:01:19 +0900

    Update mysys.py
    initial submission

--- hello-master/mysys.py
+++ hello-master/mysys.py
@@ -1,7 +1,8 @@
 import os
-
+# OS utility 성 함수를 모아놓은 부분임
+#  
 def clear():
-    # os.system('cls' if os.name == 'nt' else 'clear')
+    # os.system('cls' if os.name == 'nt' else 'clear')  # 아래 함수와  같은 기능임
 
     if os.name == 'nt':
         os.system('CLS')
