51cb154fbde32976e355f061b44e73ef76238790
pidokige02 <pidokige0204@gmail.com>
2023-04-16 14:13:11 +0900

    remove duplicated files

--- 1_excel/14_merge.py
+++
@@ -1,9 +0,0 @@
-from openpyxl import Workbook
-wb = Workbook()
-ws = wb.active
-
-# 병합하기
-ws.merge_cells("B2:D2") # B2 부터 D2 까지 합치겠음
-ws["B2"].value = "Merged Cell"
-
-wb.save("sample_merge.xlsx")
--- web_python/cgi_env.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-print('Content-Type: text/html')
-print()
-import cgi
-cgi.test()  # web server 가 cgi application에게 전달하는 data 를 표시한다.
